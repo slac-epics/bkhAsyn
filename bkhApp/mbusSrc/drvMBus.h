ce587fa2884ae0407a2a47586b50d329b6355c1b
Michael Dunning <mdunning@slac.stanford.edu>
2021-04-13 10:56:15 -0700

    More formatting cleanup

--- bkhApp/mbusSrc/drvMBus.h
+++ bkhApp/mbusSrc/drvMBus.h
@@ -35,11 +35,11 @@
 #endif
 
 #ifndef MIN
-#define MIN(a,b)        (((a)<(b))?(a):(b))
+#define MIN(a, b)        (((a)<(b))?(a):(b))
 #endif
 
 #ifndef MAX
-#define MAX(a,b)        (((a)>(b))?(a):(b))
+#define MAX(a, b)        (((a)>(b))?(a):(b))
 #endif
 
 typedef unsigned char   byte;
@@ -53,69 +53,68 @@ typedef unsigned int    uint;
 
 typedef struct{
   void*	pdrv;
-  int	addr,chan,a,rn,pix,func;
+  int	addr, chan, a, rn, pix, func;
   word	data[NDATA];
   int	len;
   int	stat;
 } iodone_t;
 
-typedef void (*iocb_t)( iodone_t);
+typedef void (*iocb_t)(iodone_t);
 
 typedef struct{
-  char port[PLEN],name[PLEN];
-  int slave,addr,len;
+  char port[PLEN], name[PLEN];
+  int slave, addr, len;
   modbusDataType_t dt;
 } drvd_t;
 
 typedef struct{
   int	maddr;
   int	func;
-  int	addr,chan,a,rn,pix;
-  int	d,len;
+  int	addr, chan, a, rn, pix;
+  int	d, len;
   int	six;		// special index of type spix_t
   void*	pdrv;
 } msgq_t;
 
-typedef enum {prioH_e,prioL_e} prio_t;
-typedef enum {normal_e,spix0_e,spix1_e,spix2_e,spix3_e,spix4_e,spix5_e} spix_t;
+typedef enum {prioH_e, prioL_e} prio_t;
+typedef enum {normal_e, spix0_e, spix1_e, spix2_e, spix3_e, spix4_e, spix5_e} spix_t;
 
 class drvMBus{
 public:
-  drvMBus( drvd_t dd,int msec);
+  drvMBus(drvd_t dd, int msec);
   void		IOThread();
-  asynStatus	mbusDoIO( prio_t prio,int six,int saddr,int addr,int chan,
-			int n,int a,int rn,int pix,int func,int len,
-			int d,void* pdrv);
+  asynStatus	mbusDoIO(prio_t prio, int six, int saddr, int addr, int chan, 
+			int n, int a, int rn, int pix, int func, int len, 
+			int d, void* pdrv);
   void          exitHndl();
   void          mbusLock();
   void          mbusUnlock();
-  void		mbusPurgeQueue( prio_t ix);
-  void		registerCB( iocb_t cb);
+  void		mbusPurgeQueue(prio_t ix);
+  void		registerCB(iocb_t cb);
   void		report();
-  void		doHist( int en);
+  void		doHist(int en);
   void		clearHist();
   epicsInt32*	getHist();
-  int		getAllowInLQ(){return(_allowInLQ);}
-  void		putAllowInLQ( int n){ _allowInLQ=n;}
+  int		getAllowInLQ() {return(_allowInLQ);}
+  void		putAllowInLQ(int n) {_allowInLQ=n;}
 
 protected:
-  void		_emptyQueue( epicsMessageQueue* pmq);
-  asynStatus	mbusMemIO( msgq_t msgq);
-  void		_doSpecial0( msgq_t msgq);
-  void		_doSpecial1( msgq_t msgq);
-  void		_doSpecial2( msgq_t msgq);
-  void		_doSpecial3( msgq_t msgq);
-  void		_doSpecial4( msgq_t msgq);
-  void		_doSpecial5( msgq_t msgq);
-  int		_readSpecialOne( msgq_t msgq,int r,epicsUInt16* v);
-  int		_writeSpecialOne( msgq_t msgq,int r,epicsUInt16 v);
-  void		_readSpecial( msgq_t msgq,int r1,int r2);
+  void		_emptyQueue(epicsMessageQueue* pmq);
+  asynStatus	mbusMemIO(msgq_t msgq);
+  void		_doSpecial0(msgq_t msgq);
+  void		_doSpecial1(msgq_t msgq);
+  void		_doSpecial2(msgq_t msgq);
+  void		_doSpecial3(msgq_t msgq);
+  void		_doSpecial4(msgq_t msgq);
+  void		_doSpecial5(msgq_t msgq);
+  int		_readSpecialOne(msgq_t msgq, int r, epicsUInt16* v);
+  int		_writeSpecialOne(msgq_t msgq, int r, epicsUInt16 v);
+  void		_readSpecial(msgq_t msgq, int r1, int r2);
   epicsMessageQueue* _pmqL;     // low priority message queue
   epicsMessageQueue* _pmqH;     // high priority message queue
   mBus_t*	_pmb;		// pointer to the modbus data structure.
 
 private:
-  asynUser *pasynUser;
   epicsMutexId  _mutexId;       // mutex for interlocking access to IO
   int		_halt;		// if true disallow IO
   iocb_t	_cb;		// IO done callback
